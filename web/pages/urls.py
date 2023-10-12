a338aeab652557f1b12fdf29e24752ddf1b69b1d
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-10-12 16:12:29 +0530

    Added contact page

--- web/pages/urls.py
+++ web/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, committee, important_dates, tracks
+from .views import home, committee, important_dates, tracks, contact
 
 urlpatterns = [
     path("", home, name="home"),
-    path("committee/",committee, name="committee"),
+    path("committee/", committee, name="committee"),
     path("important_dates/", important_dates, name="important_dates"),
-    path("tracks/", tracks, name="tracks")
-    ]
+    path("tracks/", tracks, name="tracks"),
+    path("contact/", contact, name="contact"),
+]
