466c9a44c730f6c343af23e9978fde333e568cdb
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-12-07 13:03:21 +0530

    Remove unnecessary changes

--- web/pedes/settings.py
+++ web/pedes/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("MYSQL_PASSWORD", "password"),
         "HOST": "db",
         "PORT": "3306",
-        "OPTIONS": {"init_command":  "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
