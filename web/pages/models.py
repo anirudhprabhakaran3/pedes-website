9e65d14c22ccdeb901c9f354ccde565dab6dbebc
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-12-17 11:39:39 +0530

    Add announcements

--- web/pages/models.py
+++ web/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Announcement(models.Model):
+    content = models.TextField()
+    url = models.URLField(blank=True, null=True)
--- web/pages/views.py
+++ web/pages/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
+from pages.models import Announcement
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "pages/home.html")
+    announcements = Announcement.objects.all()
 
-#committee page
+    args = {
+        "announcements": announcements
+    }
+
+    return render(request, "pages/home.html", args)
+
+
+# committee page
 
 def committee(request):
     return render(request, "pages/committee.html")
+
 
 def important_dates(request):
     return render(request, "pages/important_dates.html")
@@ -18,26 +28,34 @@ def important_dates(request):
 def tracks(request):
     return render(request, "pages/tracks.html")
 
+
 def contact(request):
     return render(request, "pages/contact.html")
 
+
 def about(request):
-    return render(request,"pages/about.html")
+    return render(request, "pages/about.html")
+
 
 def call_for_papers(request):
-    return render(request,"pages/call_for_papers.html")
+    return render(request, "pages/call_for_papers.html")
+
 
 def speakers(request):
     return render(request, "pages/speakers.html")
 
+
 def event_details(request):
     return render(request, "pages/event_details.html")
+
 
 def accomodation(request):
     return render(request, "pages/accomodation.html")
 
+
 def registration(request):
     return render(request, "pages/registration.html")
+
 
 def paper_submission(request):
     return render(request, "pages/paper_submission.html")
