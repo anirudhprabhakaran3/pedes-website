047d8b4b993565696ae83298eac3c514c29700a1
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2024-02-25 15:14:32 +0530

    Remove unnecessary logging

--- web/pedes/middleware.py
+++ web/pedes/middleware.py
@@ -1,5 +1,5 @@
 from accounts.models import ActivityLog
-from datetime import datetime
+from django.utils import timezone
 
 
 class ViewCountMiddleware:
@@ -7,13 +7,11 @@ class ViewCountMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ip = request.META.get('REMOTE_ADDR')
-        print(request.META.get('HTTP_X_REAL_IP'))
-
+        ip = request.META.get('HTTP_X_REAL_IP')
         prev_values = ActivityLog.objects.filter(ip=ip)
 
         if not prev_values.exists():
-            ActivityLog.objects.create(ip=ip, timestamp=datetime.now())
+            ActivityLog.objects.create(ip=ip, timestamp=timezone.now())
 
         response = self.get_response(request)
         return response
